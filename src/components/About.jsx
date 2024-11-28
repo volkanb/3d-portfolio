745bdbd8c0c19c6e6e83f8c953383de75f3cb6b4
volkanb <volkanbenli92@gmail.com>
2024-11-28 03:47:36 -0500

    About finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
@@ -63,4 +63,4 @@ const About = () => {
   );
 };
 
-export default About
+export default SectionWrapper(About, "about");
