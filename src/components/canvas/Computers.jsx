b7161c7be32d1175ba6a3950dc5a549f81525216
volkanb <volkanbenli92@gmail.com>
2024-11-28 03:11:01 -0500

    Hero WIP adds Computers and Loader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,13 +9,21 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <pointLight intensity={1} />
+      <hemisphereLight intensity={3} groundColor="black" />
+      <pointLight intensity={5} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
         scale={0.75}
         position={[0, -3.25, -1.5]}
-        //rotation={[-0.01, -0.2, -0.1]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
